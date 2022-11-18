a0742a03789378c9806ba04b217115a7f70bdce5
Marcelo Ferrer <marcelo@rentlysoft.com>
2022-11-18 12:00:46 +0100

    Create of the normalize MinutesMiliseconds function

--- utils/data_handler.jl
+++ utils/data_handler.jl
@@ -1,6 +1,10 @@
-function normalizeMinutesMiliseconds(dataset::AbstractArray{<:Real,2})
-    mins = minimum(dataset, dims=1)
-    maxs = maximum(dataset, dims=1)
-   
-    return convert(NTuple{2, AbstractArray{<:Real,2}}, (mins, maxs))
+# Normalize time in minutes to miliseconds
+function normalizeMinutesMiliseconds(columnToNormalize::Any)
+    for i in (1:length(columnToNormalize))
+        if columnToNormalize[i] < 100
+            columnToNormalize[i] = columnToNormalize[i] * 60000
+        end
+    end
+
+    return columnToNormalize
 end
